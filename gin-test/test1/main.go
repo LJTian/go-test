5e454d98f5d3913cb4da0258e1e67fd9fea1b2b0
LJTian <tianlijun315@gmail.com>
2021-09-30 17:59:23 +0800

    删除leetcode代码

--- gin-test/test1/main.go
+++ gin-test/test1/main.go
@@ -9,13 +9,13 @@ import (
 func main() {
 	r := gin.Default()
 	r.MaxMultipartMemory = 8 << 20
-	r.POST("/form", func(c *gin.Context) {
+	r.GET("/form", func(c *gin.Context) {
 		types := c.DefaultPostForm("type", "post")
 		username := c.PostForm("username")
 		password := c.PostForm("userpassword")
 		c.String(http.StatusOK, fmt.Sprintf("user: %s  password: %s type : %s", username, password, types))
 	})
-	r.POST("/upload", func(c *gin.Context) {
+	r.GET("/upload", func(c *gin.Context) {
 		_, file, err := c.Request.FormFile("file")
 		if err != nil {
 			c.String(http.StatusInternalServerError, "上传图片失败！")
