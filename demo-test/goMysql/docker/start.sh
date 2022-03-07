702f11843c962b38a9f7594dbe0393fdf858dc4d
ljtian <tianlijun315@gmail.com>
2022-03-07 11:47:25 +0800

    1、对脚本进行优化

--- demo-test/goMysql/docker/start.sh
+++ demo-test/goMysql/docker/start.sh
@@ -2,12 +2,24 @@
 
 echo "这是一个docker方式启动mysql的脚本"
 
-RootWd=123456
-port=13306
+echo "参数1:  容器名称(默认mysql)"
+echo "参数2:  端口号(默认13306)"
+echo "参数3:  ROOT密码(默认123456)"
+
 DockerName=$1
+port=$2
+RootWd=$3
 
 if [ "1${DockerName}" = "1" ] ; then
 	DockerName=mysql
+fi
+
+if [ "1${port}" = "1" ]; then
+  port=13306
+fi
+
+if [ "1${RootWd}" = "1" ]; then
+  RootWd=123456
 fi
 
 docker run --name=${DockerName} -it -p 13306:3306 -e MYSQL_ROOT_PASSWORD=${RootWd} -d mariadb
@@ -27,7 +39,7 @@ echo "docker exec -it ${DockerId} bash"
 
 ## 登录mysql
 echo "mysql -u root -p "
-echo "设置的牧人密码为:[123456]"
+echo "设置的默认密码为:[123456]"
 ## 授权
 echo "grant all privileges on *.*  to 'root'@'%';"
 echo "flush privileges;"
