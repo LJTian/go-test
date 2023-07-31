202fc106a512ca3f5b0748343bb887a188ad4099
tianlijun <tianlijun@uniontech.com>
2023-07-31 17:37:55 +0800

    1

--- demo-test/indexString/main.go
+++ demo-test/indexString/main.go
@@ -10,5 +10,8 @@ func main() {
 	str1 := "TLJ_test测试商户#as##sada"
 
 	i := strings.Split(str1, "#")
-	fmt.Printf("str1 is [%s]", i[0])
+	fmt.Printf("str1 is [%s]\n", i[0])
+
+	imageName := "registry.uniontech.com/utccp-components/docker-builder:1.2.1-rc9"
+	fmt.Println(imageName[:strings.LastIndexByte(imageName, '/')] + "/" + "abc" + imageName[strings.LastIndexByte(imageName, ':'):])
 }
