a5799e9374a86b9eae2059f8c693146f7dc0a028
ljtian <tianlijun315@gmail.com>
2023-08-04 16:55:18 +0800

    1、添加测试代码，实现 web 展示 go-echarts 生成的图

---
+++ go-echarts-test/test1/main.go
@@ -0,0 +1,49 @@
+// example.go
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-echarts/go-echarts/charts"
+)
+
+var nameItems = []string{"衬衫", "牛仔裤", "运动裤", "袜子", "冲锋衣", "羊毛衫"}
+var seed = rand.NewSource(time.Now().UnixNano())
+
+func randInt() []int {
+	cnt := len(nameItems)
+	r := make([]int, 0)
+	for i := 0; i < cnt; i++ {
+		r = append(r, int(seed.Int63())%50)
+	}
+	return r
+}
+
+func main() {
+
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.TitleOpts{Title: "Bar-示例图"}, charts.ToolboxOpts{Show: true})
+	bar.AddXAxis(nameItems).
+		AddYAxis("商家A", randInt()).
+		AddYAxis("商家B", randInt())
+	f, err := os.Create("bar.html")
+	if err != nil {
+		log.Println(err)
+	}
+	bar.Render(f)
+
+	router := gin.Default()
+	router.LoadHTMLGlob("./*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "bar.html", gin.H{
+			"title": "Main website",
+		})
+	})
+	router.Run(":8080")
+}
